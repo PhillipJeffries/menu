d269f85f8aa422d643465d0e8266274f96a497ae
dmitry <ggbox@mail.ru>
2022-10-18 23:03:11 +0400

    cors-headers

--- menu/models.py
+++ menu/models.py
@@ -1,11 +1,19 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 
 class Category(models.Model):
     category = models.CharField("category", max_length=50)
+    
     def __str__(self):
         return self.category
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+
+
 
 class Dish(models.Model):
     name = models.CharField("dish", max_length=200)
@@ -13,3 +21,7 @@ class Dish(models.Model):
     category = models.ManyToManyField(Category)
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = "dish"
+        verbose_name_plural = "dishes"
